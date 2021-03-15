08b3f2370e701e96d6332f6c2897e41ee3ad32d5
Doring <hojun.dev@gmail.com>
2021-03-16 02:39:45 +0900

    전투시스템 구현 완료
    전투시스템 제작 완료. 게임 플레이 가능합니다.
    아직 스킬의 벨런스조정은 하지 않았습니다.

--- VSgame.py
+++ VSgame.py
@@ -5,10 +5,10 @@ import random # 랜덤수 함수 포함
 
 #변수 및 함수-------------------------------------------------
 #데이터#
-default_player_hp = 1000
-default_player_atk = 100
-default_monster_hp = 2000
-default_monster_atk = 50
+default_player_hp = 200
+default_player_atk = 50
+default_monster_hp = 500
+default_monster_atk = 40
 player_hp = 0
 player_atk = 0
 monster_hp = 0
@@ -19,7 +19,7 @@ turn = 0 #현재 턴 수
 
 #게임메뉴#
 def Mainmenu(): #메인화면
-    print('[MAIN] VS Game \\\\ Python Game Project by VDoring')
+    print('\n\n[MAIN] VS Game \\\\ Python Game Project by VDoring')
     print('메뉴를 선택해주세요.\n[1]시작\t\t[2]초기화\t[3]종료')
     user_input = int(input())
     if user_input == 1:
@@ -55,7 +55,7 @@ def ResetData(): #2.데이터초기화
     monster_hp = default_monster_hp
     monster_atk = default_monster_atk
     turn = 0
-    print('데이터초기화 완료\n\n')
+    print('> 데이터초기화 완료 <\n\n')
 
 def Exit(): #3.게임종료
     print('플레이해주셔서 감사해요!')
@@ -66,64 +66,123 @@ def Exit(): #3.게임종료
 def PlayerActSelect(): #플레이어의 턴 행동선택
     global turn
     turn += 1
-    print('무엇을 할까?\t(턴:%d)'%turn)
+    global is_player_restrain
+    if is_player_restrain == 1 or is_player_restrain == 2:
+        print('\n당신은 움직일 수 없다!')
+        is_player_restrain -= 1
+        MonsterActSelect()
+
+    print('\n\n---------------무엇을 할까? - (턴:%d)---------------'%turn)
     print('1.물리공격   2.마법공격   3.구속')
     user_act = int(input())
     if user_act == 1:
         PlayerActPhysicalAttack()
     elif user_act == 2:
-        PlayerActMagicAttack()
+        PlayerActMagicialAttack()
     elif user_act == 3:
         PlayerActBind()
     else:
         print('다시 입력해주세요.')
+        turn -= 1
         PlayerActSelect()
-    print('남은 몬스터의 HP:',monster_hp)
-    #MonsterActSelect()
+
+    time.sleep(0.8)
+    if monster_hp >= 1:
+        print('\n남은 몬스터의 HP:',monster_hp)
+        time.sleep(1)
+        MonsterActSelect()
+    else:
+        print('몬스터가 쓰러졌습니다!')
+        print('메인화면으로 돌아갑니다.')
+        time.sleep(2)
+        Mainmenu()
 
 
 def PlayerActPhysicalAttack():
-    print('플레이어는 상대를 배었다!')
+    print('\n당신은 상대를 배었다!')
     global monster_hp
     global player_atk
-    damage = int(player_atk * 0.7)
+    damage = int(player_atk * 1.1)
     monster_hp -= damage
+    time.sleep(0.8)
     print('몬스터는 %d의 피해를 입었다!' %damage)
 
-def PlayerActMagicAttack():
-    print('플레이어는 전격공격을 하였다!')
+def PlayerActMagicialAttack():
+    print('\n당신은 전격공격을 하였다!')
     global monster_hp
     global player_atk
     damage = int(player_atk * 0.5)
     monster_hp -= damage
+    time.sleep(0.8)
     print('몬스터는 %d의 피해를 입었다!' %damage)
 
 def PlayerActBind():
-    print('플레이어는 구속을 시도했다. ')
+    print('\n당신은 구속을 시도했다. ')
+    global is_monster_restrain
     monster_restrain_percent = random.randint(0,10)
     time.sleep(1)
     if monster_restrain_percent >= 8:
         print('성공!')
-        global is_monster_restrain
-        is_monster_restrain = 1
+        is_monster_restrain = 2
     else:
         print('실패했다...')
 
 
 def MonsterActSelect():
     global is_monster_restrain
-    if is_monster_restrain == 1:
-        print('몬스터는 움직일 수 없다!')
-        return
-    monster_act = random.random(1,4)
+    if is_monster_restrain == 1 or is_monster_restrain == 2:
+        print('\n몬스터는 움직일 수 없다!')
+        is_monster_restrain -= 1
+        PlayerActSelect()
+
+    print('\n\n-----------------상대의 턴 ------------------')
+    monster_act = random.randint(1,3)
     if monster_act == 1:
-        print('몬스터는 주먹을 내질렀다!')
+        MonsterActPhysicalAttack()
     elif monster_act == 2:
-        print('몬스터는 파이어볼을 당신에게 던졌다!')
+        MonsterActMagicalAttack()
     elif monster_act == 3:
-        print('몬스터는 덩쿨을 손에 들었다.')
+        MonsterActBind()
 
+    time.sleep(0.8)
+    if player_hp >= 1:
+        print('\n남은 플레이어의 HP:',player_hp)
+        time.sleep(1)
+        PlayerActSelect()
+    else:
+        print('플레이어가 쓰러졌습니다!')
+        print('메인화면으로 돌아갑니다.')
+        time.sleep(2)
+        Mainmenu()
 
+def MonsterActPhysicalAttack():
+    print('몬스터는 주먹을 내질렀다!')
+    global player_hp
+    global monster_atk
+    damage = int(monster_atk * 0.8)
+    player_hp -= damage
+    time.sleep(0.8)
+    print('당신은 %d의 피해를 입었다!' %damage)
+
+def MonsterActMagicalAttack():
+    print('몬스터는 에너지를 방출했다!')
+    global player_hp
+    global monster_atk
+    damage = int(monster_atk * 0.6)
+    player_hp -= damage
+    time.sleep(0.8)
+    print('당신은 %d의 피해를 입었다!' %damage)
+
+def MonsterActBind():
+    print('몬스터는 덩쿨을 날렸다!')
+    global is_player_restrain
+    player_restrain_percent = random.randint(0,10)
+    time.sleep(1)
+    if player_restrain_percent >= 7:
+        print('성공.')
+        is_player_restrain = 2
+    else:
+        print('실패했다!')
 
 #메인코드-----------------------------------------------------
 Mainmenu()
